02d2499e094d2828b696a39485c0f9755d0b9e1e
zebwul <zebastian.wulff@live.se>
2026-04-03 12:15:08 +0200

    Setup monaco editor and removed react/vite example code

---
+++ src/components/CodeEditor.jsx
@@ -0,0 +1,16 @@
+import Editor from "@monaco-editor/react";
+
+function CodeEditor() {
+  return (
+    <div className="editor-container">
+      <Editor
+        defaultLanguage="javascript"
+        defaultValue='console.log("Hello World");'
+        theme="vs-dark"
+        height="100%"
+      />
+    </div>
+  );
+}
+
+export default CodeEditor;
