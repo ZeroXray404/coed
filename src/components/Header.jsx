b05df3a236b5af5bb4e96263140762984cdf99f5
Yakowboi <ariannawas@gmail.com>
2026-04-10 20:22:07 +0200

    Add Header.jsx component and use in App.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <section className="header">
+      <div className="quick-actions"></div>
+      <div className="search-bar"></div>
+      <div className="user-btns"></div>
+    </section>
+  )
+}
+
+export default Header
