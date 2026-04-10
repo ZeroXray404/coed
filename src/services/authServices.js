b978e83fe9343152830c726b7aa9aa67d528d73d
zebwul <zebastian.wulff@live.se>
2026-04-10 15:24:52 +0200

    Add: ignore on .env.local file. Add: apiConfig and strucure for furture development with API

---
+++ src/services/authServices.js
@@ -0,0 +1,11 @@
+// Läser in API-konfigurationen
+import { getApiConfig } from './apiConfig.js'
+const { API_KEY, BASE_URL } = getApiConfig()
+
+// Registera användare
+
+// Logga in användare
+
+// Logga ut användare
+
+// Kontrollera om användare är inloggad
