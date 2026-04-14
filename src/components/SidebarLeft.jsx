e44369dc4106ca3c037d757b7604ea7219e3f0b2
ZeroXray404 <dev.0x404@gmail.com>
2026-04-14 21:53:11 +0200

    feat(sidebar) Add sidebar component

---
+++ src/components/SidebarLeft.jsx
@@ -0,0 +1,55 @@
+function SidebarLeft() {
+  return (
+    <section className="sidebar-left">
+      <div className="sidebar-header">
+        <h3>Sidebar Header</h3>
+      </div>
+      <div className="sidebar-content">
+        <h3>Sidebar Left</h3>
+        {/* Placeholders för att testa sidebar-content vid flertal filer */}
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+        <p>Left sidebar content.</p>
+      </div>
+      <div className="sidebar-footer">
+        <h3>Sidebar Footer</h3>
+      </div>
+    </section>
+  )
+}
+
+// const demoTree = [
+//   //Demoträd för filstruktur
+//   {
+//     id: 'folder-1',
+//     name: 'scr',
+//     kind: 'folder',
+//     children: [
+//       { id: 'file-1', name: 'App.jsx', kind: 'file' },
+//       { id: 'file-2', name: 'main.jsx', kind: 'file' },
+//     ],
+//   },
+//   {
+//     id: 'file-3',
+//     name: 'index.html',
+//     kind: 'file',
+//   },
+// ]
+
+export default SidebarLeft
