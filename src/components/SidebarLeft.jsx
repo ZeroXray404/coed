e8fbdb00d5f7c3ac1e843d6b63dc76a48d226aad
ZeroXray404 <dev.0x404@gmail.com>
2026-04-15 15:56:03 +0200

    feat(components): Add component structure for SidebarLeft.jsx

--- src/components/SidebarLeft.jsx
+++ src/components/SidebarLeft.jsx
@@ -1,55 +1,66 @@
-function SidebarLeft() {
+function SidebarHeader() {
   return (
-    <section className="sidebar-left">
-      <div className="sidebar-header">
-        <h3>Sidebar Header</h3>
-      </div>
-      <div className="sidebar-content">
-        <h3>Sidebar Left</h3>
-        {/* Placeholders för att testa sidebar-content vid flertal filer */}
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-        <p>Left sidebar content.</p>
-      </div>
-      <div className="sidebar-footer">
-        <h3>Sidebar Footer</h3>
-      </div>
-    </section>
+    <div className="sidebar-header">
+      <h3>Sidebar Header</h3>
+    </div>
   )
 }
 
-// const demoTree = [
-//   //Demoträd för filstruktur
-//   {
-//     id: 'folder-1',
-//     name: 'scr',
-//     kind: 'folder',
-//     children: [
-//       { id: 'file-1', name: 'App.jsx', kind: 'file' },
-//       { id: 'file-2', name: 'main.jsx', kind: 'file' },
-//     ],
-//   },
-//   {
-//     id: 'file-3',
-//     name: 'index.html',
-//     kind: 'file',
-//   },
-// ]
+function SidebarContent() {
+  return (
+    <div className="sidebar-content">
+      <h3>Sidebar Content</h3>
+      <ul>
+        {demoProjects.data.map((project) => (
+          <li key={project.uid}>{project.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+function SidebarFooter() {
+  return (
+    <div className="sidebar-footer">
+      <h3>Sidebar Footer</h3>
+    </div>
+  )
+}
+
+function SidebarLeft() {
+  return (
+    <div className="sidebar-left">
+      <SidebarHeader />
+      <SidebarContent />
+      <SidebarFooter />
+    </div>
+  )
+}
+
+const demoProjects = {
+  // Mockad data för projekten i sidofältet.
+  data: [
+    {
+      uid: 'project-1',
+      name: 'Starter project',
+    },
+    {
+      uid: 'project-2',
+      name: 'Greatest project ever',
+    },
+    {
+      uid: 'project-3',
+      name: 'Another project',
+    },
+    {
+      uid: 'project-4',
+      name: 'Yet another project',
+    },
+    {
+      uid: 'project-5',
+      name: 'Yet another project',
+    },
+  ],
+}
 
 export default SidebarLeft
