df9c4756eb71c657294d83fa3164083acdc303dc
ZeroXray404 <dev.0x404@gmail.com>
2026-04-29 18:24:55 +0200

    feat(services): add fileServices containing api calls to docket

---
+++ src/services/fileServices.js
@@ -0,0 +1,16 @@
+import { requireApiConfig } from './apiConfig.js'
+import { getToken } from './authServices.js'
+
+export async function getAllProjects() {}
+
+export async function getProjectWithFilesAndUsers(/*projectId*/) {}
+
+export function createProject() {}
+
+export function addUserToProject() {}
+
+export function deleteProjectWithFiles() {}
+
+export function createFile() {}
+
+export function deleteFile() {}
