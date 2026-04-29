9b99c66a69c967b74662ae1068e6d6c1dd73732f
ZeroXray404 <dev.0x404@gmail.com>
2026-04-29 21:44:37 +0200

    feat(services): add fetch functions to manage fileservices

--- src/services/fileServices.js
+++ src/services/fileServices.js
@@ -1,16 +1,70 @@
 import { requireApiConfig } from './apiConfig.js'
 import { getToken } from './authServices.js'
 
-export async function getAllProjects() {}
+export async function getAllProjects() {
+  const { DOCKET_BASE_URL } = requireApiConfig()
+  const url = `${DOCKET_BASE_URL}/projects`
+  const TOKEN = getToken()
 
-export async function getProjectWithFilesAndUsers(/*projectId*/) {}
+  const response = await fetch(url, {
+    headers: {
+      'x-access-token': [TOKEN],
+    },
+  })
+  const result = await response.json()
 
-export function createProject() {}
+  if (!response.ok) {
+    throw new Error(result?.errors?.detail || 'Failed to get projects')
+  }
 
-export function addUserToProject() {}
+  return result
+}
 
-export function deleteProjectWithFiles() {}
+export async function getProjectWithFilesAndUsers(uid) {
+  const { DOCKET_BASE_URL } = requireApiConfig()
+  const url = `${DOCKET_BASE_URL}/projects/${uid}`
+  const TOKEN = getToken()
 
-export function createFile() {}
+  const response = await fetch(url, {
+    headers: {
+      'x-access-token': [TOKEN],
+    },
+  })
+  const result = await response.json()
 
-export function deleteFile() {}
+  if (!response.ok) {
+    throw new Error(
+      result?.errors?.detail || 'Failed to get project with files & users'
+    )
+  }
+  return result
+}
+
+export async function createProject(name) {
+  const { DOCKET_BASE_URL } = requireApiConfig()
+  const url = `${DOCKET_BASE_URL}/projects/`
+  const TOKEN = getToken()
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'x-access-token': [TOKEN],
+    },
+    body: JSON.stringify({ name }),
+  })
+  const result = await response.json()
+
+  if (!response.ok) {
+    throw new Error(result?.errors?.detail || 'Failed to create project')
+  }
+  return result
+}
+
+// export async function addUserToProject() {}
+
+// export async function deleteProjectWithFiles() {}
+
+// export async function createFile() {}
+
+// export async function deleteFile() {}
