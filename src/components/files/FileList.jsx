b37ec2758ab3f500d7492fa523722628803c07e7
ZeroXray404 <dev.0x404@gmail.com>
2026-05-06 22:15:29 +0200

    feat(display projects): returns projects in FileList component
    inherrits props, filters projects out empty string in key=name & maps projects

--- src/components/files/FileList.jsx
+++ src/components/files/FileList.jsx
@@ -1,6 +1,13 @@
 import { AppWindow } from 'lucide-react'
 
-function FileListContent({ deleteMode, selectedFiles, setSelectedFiles }) {
+function FileListContent({
+  deleteMode,
+  selectedFiles,
+  setSelectedFiles,
+  projects,
+  isLoading,
+  error,
+}) {
   /*
    * prev = current selected IDs
    * includes(id) = check if selected (if ID exists)
@@ -11,61 +18,34 @@ function FileListContent({ deleteMode, selectedFiles, setSelectedFiles }) {
     setSelectedFiles((prev) =>
       prev.includes(id) ? prev.filter((x) => x !== id) : [...prev, id]
     )
-
-    if (0 < selectedFiles.length < 1) {
-      alert('MODAL WINDOW HERE')
-    }
   }
 
   return (
     <div className="sidebar-content">
+      {isLoading && <p>Loading projects...</p>}
+      {error && <p>{error}</p>}
       <ul>
-        {demoProjects.data.map((project) => {
-          const isSelected = selectedFiles.includes(project.uid)
-
-          return (
-            <li key={project.uid} className={isSelected ? 'selected' : ''}>
-              <AppWindow size={16} />
-              {project.name}
-              <input
-                type="checkbox"
-                name="fileSelect"
-                className={deleteMode ? 'active delete-mode' : ''}
-                checked={isSelected}
-                onChange={() => toggle(project.uid)}
-              />
-            </li>
-          )
-        })}
+        {projects
+          .filter((project) => project.name && project.name.trim() !== '')
+          .map((project) => {
+            const isSelected = selectedFiles.includes(project.uid)
+            return (
+              <li key={project.uid} className={isSelected ? 'selected' : ''}>
+                <AppWindow size={16} />
+                {project.name}
+                <input
+                  type="checkbox"
+                  name="fileSelect"
+                  className={deleteMode ? 'active delete-mode' : ''}
+                  checked={isSelected}
+                  onChange={() => toggle(project.uid)}
+                />
+              </li>
+            )
+          })}
       </ul>
     </div>
   )
-}
-
-const demoProjects = {
-  // Mockad data för projekten i sidofältet.
-  data: [
-    {
-      uid: 'project-1',
-      name: 'Starter project',
-    },
-    {
-      uid: 'project-2',
-      name: 'Greatest project ever',
-    },
-    {
-      uid: 'project-3',
-      name: 'Another project',
-    },
-    {
-      uid: 'project-4',
-      name: 'Yet another project',
-    },
-    {
-      uid: 'project-5',
-      name: 'Yet another project',
-    },
-  ],
 }
 
 export default FileListContent
