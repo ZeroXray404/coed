bc202db4160b8396bfcaf5ce1bf6593af2e17ccc
Yakowboi <ariannawas@gmail.com>
2026-05-24 23:12:29 +0200

    refactor(removeusermodal): fix styling issues and shorten the text

--- src/components/files/FileList/RemoveUserModal.jsx
+++ src/components/files/FileList/RemoveUserModal.jsx
@@ -4,33 +4,32 @@ function RemoveUserModal({ project, isOpen, onConfirm, onCancel }) {
   }
 
   return (
-    <div className="sidebar-content">
-      <div className="delete-modal">
-        <div className="delete-modal-text">
-          <h1>Leave Project?</h1>
-          <p>Are you sure you want to leave project?</p>
-          <p>If you want to regain access you</p>
-          <p>have to get invited again.</p>
-          <p>Leave: {project.name}</p>
-        </div>
-        <div className="delete-modal-btns">
-          <button
-            type="button"
-            className="confirm"
-            onClick={onConfirm}
-            aria-label="Confirm leaving project"
-          >
-            Leave
-          </button>
-          <button
-            type="button"
-            className="cancel"
-            onClick={onCancel}
-            aria-label="Cancel leaving project"
-          >
-            Cancel
-          </button>
-        </div>
+    <div className="delete-modal">
+      <div className="delete-modal-text">
+        <h1>Leave Project?</h1>
+        <p>Are you sure you want to leave '{project.name}'?</p>
+        <p>
+          Once you leave, you'll lose access to this project and must be
+          re-invited to rejoin.
+        </p>
+      </div>
+      <div className="delete-modal-btns">
+        <button
+          type="button"
+          className="confirm"
+          onClick={onConfirm}
+          aria-label="Confirm leaving project"
+        >
+          Leave
+        </button>
+        <button
+          type="button"
+          className="cancel"
+          onClick={onCancel}
+          aria-label="Cancel leaving project"
+        >
+          Cancel
+        </button>
       </div>
     </div>
   )
